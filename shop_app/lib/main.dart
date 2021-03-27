7e83f4ca03acfb7c1ed8dbafed1953fa39210c1d
Alfonso <alfo.di.pace@gmail.com>
2021-03-27 11:59:04 +0100

    9.4 added manage products page

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -10,6 +10,7 @@ import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -40,6 +41,7 @@ class MyApp extends StatelessWidget {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
             CartScreen.routeName: (ctx) => CartScreen(),
             OrdersScreen.routeName: (ctx) => OrdersScreen(),
+            UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
           }),
     );
   }
---
+++ shop_app/lib/screens/user_products_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widgets/app_drawer.dart';
+import 'package:flutter_complete_guide/widgets/user_product_item.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
